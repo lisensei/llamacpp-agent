c78042de2e99e3db90246d6d1bd180bdeb17d0bb
lisensei <sherlocksolution@gmail.com>
2024-05-19 22:10:51 +0300

    use Llama 70b

--- api.py
+++ api.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask,request
 from langchain_community.llms import LlamaCpp
 from langchain.agents import create_react_agent,AgentExecutor
 from langchain_community.tools.tavily_search import TavilySearchResults
-from langchain_core.prompts import PromptTemplate
+from langchain_core.prompts import PromptTemplate,ChatPromptTemplate
 from argparse import ArgumentParser
 import langchain
+from langchain_groq import ChatGroq
 langchain.debug=True
 parser=ArgumentParser()
 parser.add_argument("-model_path",type=str)
@@ -13,15 +14,16 @@ parser.add_argument("-context_length",type=int,default=4096)
 parser.add_argument("-temperature",type=float,default=1)
 parser.add_argument("-max_tokens",type=int,default=4096)
 parser.add_argument("-question",type=str)
+parser.add_argument("-local_model",type=int,default=0)
 args=parser.parse_args()
 app=Flask(__name__)
 app.config["model_path"]=args.model_path
-bot="<|begin_of_text|>"
-shi="<|start_header_id|>"
-ehi="<|end_header_id|>"
-eot="<|eot_id|>"
-temp='''
-{bot}{shi}system{ehi}You are an accurate intelligent system. You one and only task is answer any questions. You must use this tool: {tools}, to search for information. 
+app.config["local_model"]=args.local_model
+ids={"bot":"<|begin_of_text|>",
+"shi":"<|start_header_id|>",
+"ehi":"<|end_header_id|>",
+"eot":"<|eot_id|>"}
+temp='''You are an accurate intelligent system. You one and only task is answer any questions. You must use this tool: {tools}, to search for information. 
 The name of the tool is: {tool_names}.
 
 You should use the following pattern to answer questions:
@@ -33,7 +35,7 @@ Action Input: The parameters passed to the action
 Observation: The result of taking the action
 Final Answer: The final answer to the question
 
-The Thought-Action-Action Input-Observataion process can repeat at most 2 times. When you can't find the answer, return results not found.
+The Thought-Action-Action Input-Observataion process can repeat at most 2 times. When you can't find the answer, summarize the information you got and provide the website links of such information.
 
 
 Here are two examples:
@@ -56,13 +58,34 @@ Remember, the tool's name is case sensitive, you must use this exact tool name: 
 
 Start!
 {eot}
-{shi}intelligent system{ehi}
-Question: {input}
-{agent_scratchpad}
 '''
-model=LlamaCpp(model_path=app.config["model_path"],n_gpu_layers=args.gpu_layers,n_ctx=args.context_length,max_tokens=args.max_tokens,temperature=args.temperature)
-tools=[TavilySearchResults(max_results=1)]
-prompt=PromptTemplate.from_template(temp)
-age=create_react_agent(model,tools,prompt)
-agent=AgentExecutor(agent=age,tools=tools,verbose=True,handle_parsing_errors=True)
-agent.invoke({"input":args.question,"bot":bot,"shi":shi,"ehi":ehi,"eot":eot})
+assistant='''Question: {input}
+{agent_scratchpad}'''
+@app.route("/api/v1")
+def streaming():
+    if app.config["local_model"]:
+        model=LlamaCpp(model_path=app.config["model_path"],n_gpu_layers=args.gpu_layers,n_ctx=args.context_length,max_tokens=args.max_tokens,temperature=args.temperature)
+        prompt=PromptTemplate.from_template('''{bot}{shi}system{ehi}'''+temp+'''{shi}intelligent system{ehi}'''+assistant)
+    else:
+        model=ChatGroq(temperature=0,model="llama3-70b-8192")
+        prompt=ChatPromptTemplate.from_messages([("system",temp),("assistant",assistant)])
+
+    question=request.json["question"]
+    tools=[TavilySearchResults(max_results=1)]
+    age=create_react_agent(model,tools,prompt)
+    agent=AgentExecutor(agent=age,tools=tools,verbose=True,handle_parsing_errors=True)
+    if isinstance(prompt,ChatPromptTemplate):
+        ids={"bot":"",
+        "shi":"",
+        "ehi":"",
+        "eot":""}
+    else:
+        ids={"bot":"<|begin_of_text|>",
+        "shi":"<|start_header_id|>",
+        "ehi":"<|end_header_id|>",
+        "eot":"<|eot_id|>"}
+    result=agent.invoke(ids|{"input":question})
+    return result["output"],200
+
+if __name__ == "__main__":
+    app.run(debug=True,host="0.0.0.0",port=6000)
